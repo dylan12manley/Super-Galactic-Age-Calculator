f4f0aa14f66464a9b088301b9893e728826d4d8d
Dylan Manley <dylan.manley@gmail.com>
2019-11-15 11:13:48 -0800

    add inputs filled test

--- __Tests__/User.test.js
+++ __Tests__/User.test.js
@@ -1,11 +1,17 @@
 import { User } from './../src/User.js';
 
 describe('User', () => {
+
   test('should determine user inputs a positive interger', () => {
    let user = new User(-10, 80);
    console.log(user.age);
    expect(user.intergerCheck()).toEqual("How can you live in negative years?");
  });
+ test('should determine user inputs are filled', () => {
+  let user = new User(10, 80);
+  console.log(user.age);
+  expect(user.inputCheck()).toEqual("You gotta enter an number for both inputs");
+});
 });
 //  test('should determine *spec 1*', () => {
 //   let lilConstr = new Constructor("strings and numbers like", 5, objects, [empty arrays]);
