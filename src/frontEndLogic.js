be962b5eb5b4fed0cd94b773675c6dbd5255c308
Dylan Manley <dylan.manley@gmail.com>
2019-11-15 10:44:49 -0800

    update import

--- src/frontEndLogic.js
+++ src/frontEndLogic.js
@@ -1,4 +1,4 @@
-import { calculator } from './../src/calculator.js';
+import { User } from './../src/User.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
